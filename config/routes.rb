ab766eb0dbfca6a5ba0e2b38c5e7320068dfc21f
Xiaoli Liang <xliang6@gmail.com>
2012-02-14 08:10:38 -0800

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 SampleApp::Application.routes.draw do
 
+  get "users/new"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
+  match '/signup', :to => 'users#new'
+
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
